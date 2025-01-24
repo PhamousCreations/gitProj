54a3679288d27017837195f2095b5213ef51e61f
PhamousCreations <158514325+PhamousCreations@users.noreply.github.com>
2025-01-24 16:55:38 +0000

    first commit

--- area.py
+++ area.py
@@ -1,36 +1,17 @@
 ##Finding the area of a rectangular room.'
-from modulefinder import ReplacePackage
-
-import turtle as t
 
 length = input("What is the length of the room: ")
 print("")
-print("")
 width = input("What is the width of the room: ")
-print("")
 print("")
 area = float(length) * float(width)
 print("Calculating........")
 print(".......")
 print("The area of the room is: ", float(area))
 print("")
-j = 10
-k = 2
-l = 4
-j = j * k
-k = k * l
-k = j + l
-print(j)
-print(k)
-print(l)
-print("")
+
 one_acre = 43560
 numb_acre = 391876 / one_acre
 print(numb_acre)
-print("")
-print("Your name: ")
-print("Your address, with city, state, and ZIP code: ")
-print("Your telephone number: ")
-print("Your college major: ")
 
 t.exitonclick()
